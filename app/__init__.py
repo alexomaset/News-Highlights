8dbbae611ac467978919ddafb2ff29832a238aef
alexomaset <alexomaset711@gmail.com>
2019-04-14 22:49:44 +0300

    modified the configurations file

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,12 @@ from .config import config_options
 bootstrap = Bootstrap()
 
 def create_app(config_name):
+    """
+    Intializing the app
+    Function that takes configuration setting key as an argument
+    Args:
+        config_name: name of configuration used
+    """
     app = Flask(__name__)
 
     # Creating app configurations
