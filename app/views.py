fc13dcf3f3209626b519e35c79f7bc7509da4ea0
alexomaset <alexomaset711@gmail.com>
2019-04-14 12:34:52 +0300

    modified and added the con files

--- app/views.py
+++ app/views.py
@@ -8,5 +8,6 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
+    title
     message = 'Hello World'
     return render_template('index.html',message = message)
